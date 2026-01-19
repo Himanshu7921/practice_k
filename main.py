2a39203f23fd80832565d7355a3b883813c50186
Himanshu7921 <himanshusr451tehs@gmail.com>
2026-01-19 22:40:48 +0530

    Added Functions for sending Money from one account to another

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-p1 = 30
+def transferToAccount(p1, p2, how_much_to_send):
+    if p1 > how_much_to_send:
+        print("Money before Sedning...")
+        print(f"p1: {p1}")
+        print(f"p2: {p2}")
+
+        # Money Sending Logic
+        p1 = p1 - how_much_to_send
+        p2 = p2 + how_much_to_send
+
+        print(f"Money after Sending...")
+        print(f"p1: {p1}")
+        print(f"p2: {p2}")
+    else:
+        print("Unable to send Money, Insufficient Balance!")
+    return p1, p2
+
+p1 = 100
 p2 = 130
+send_money = 30
 
-print("Money before Sedning...")
-print(f"p1: {p1}")
-print(f"p2: {p2}")
-send_money = 31
-
-if p1 <send_money:
-    print("cannot send money")
-else:
-
-
-    p1 = p1 - send_money
-    p2 = p2 + send_money
-
-    print("Money after Sedning...")
-    print(f"p1: {p1}")
-    print(f"p2: {p2}")
+p1, p2 = transferToAccount(p1, p2, send_money)
+print("Sending Again")
+p1, p2 = transferToAccount(p1, p2, send_money)
