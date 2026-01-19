fd01a90bfc9f11eec4fd1fc97d1c2f66c2fe67f6
Himanshu7921 <himanshusr451tehs@gmail.com>
2026-01-19 22:05:30 +0530

    Added Basic Logic of Sending and Receving Money

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# code likhega tu yaha
+p1 = 0
+p2 = 130
+
+print("Money before Sedning...")
+print(f"p1: {p1}")
+print(f"p2: {p2}")
+
+send_money = 30
+p1 = p1 - send_money
+p2 = p2 + send_money
+
+print("Money after Sedning...")
+print(f"p1: {p1}")
+print(f"p2: {p2}")
